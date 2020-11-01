4a1bfbbd919f46b34d48e812679e55bfbb5c237d
Joost Verbraeken <j.verbraeken@student.tudelft.nl>
2020-11-01 15:24:09 +0100

    Create script to clear log files

--- SetupPorts.bat
+++ SetupPorts.bat
@@ -2,16 +2,16 @@
 set ports[0]=5554
 set ports[1]=5556
 set ports[2]=5558
-set redirects[0]=65460
-set redirects[1]=65451
-set redirects[2]=59001
+set redirects[0]=59235
+set redirects[1]=64848
+set redirects[2]=59243
 
 set "x=0"
 :SymLoop
 if defined ports[%x%] (
 	call echo %%ports[%x%]%%
-    set /a "x+=1"
-    GOTO :SymLoop 
+	set /a "x+=1"
+	GOTO :SymLoop 
 )
 set /a "x-=1"
 echo "Redirecting %x% AVDs"
@@ -20,6 +20,6 @@ setlocal EnableDelayedExpansion
 for /L %%i in (0, 1, %x%) do (
 	echo Forwarding AVD !ports[%%i]! to port !redirects[%%i]!
 	start telnet.exe localhost !ports[%%i]!
-	cscript TelnetHelper.vbs !redirects[%%i]!
+	cscript SetupPortsHelper.vbs !redirects[%%i]!
 )
 endlocal
--- SetupPortsHelper.vbs
+++ SetupPortsHelper.vbs
@@ -1,7 +1,8 @@
 set OBJECT=WScript.CreateObject("WScript.Shell")
-WScript.sleep 50
+WScript.sleep 25
 OBJECT.SendKeys "auth fqvo8zH1j32aFoVB{ENTER}"
-WScript.sleep 50
+WScript.sleep 25
 OBJECT.SendKeys "redir add udp:" & WScript.Arguments.Item(0) & ":8090{ENTER}"
+WScript.sleep 25
 'WScript.sleep 1500
 'OBJECT.SendKeys "exit{ENTER}" 
